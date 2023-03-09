fcbe5c3131faf85e0872a4675528f5b129d50878
DaraKolm <75583812+DaraKolm@users.noreply.github.com>
2023-03-09 11:59:08 +0200

    change pom teacher service

--- services/teacher/src/main/java/com/example/teacher/models/TeacherEntity.java
+++ services/teacher/src/main/java/com/example/teacher/models/TeacherEntity.java
@@ -1,10 +1,15 @@
 package com.example.teacher.models;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+/*
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.Id;*/
 import java.util.Objects;
 
 @Entity
