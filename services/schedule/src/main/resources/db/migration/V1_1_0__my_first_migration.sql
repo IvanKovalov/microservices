c6e3dabca2503c9489ba7f2f29512944aa577b97
IvanKovalov <kovalevvana8@gmail.com>
2023-03-26 20:52:51 +0300

    + add flyway

---
+++ services/schedule/src/main/resources/db/migration/V1_1_0__my_first_migration.sql
@@ -0,0 +1,5 @@
+
+
+
+insert into schedule_entity (class_id, student_id, subject, teacher_id) values (2, 1, 'Lesson', 2);
+insert into schedule_entity (class_id, student_id, subject, teacher_id) values (5, 2, 'Example', 3);
