51d7dc4a07357c39c628f37add2b13c6368e69a0
IvanKovalov <kovalevvana8@gmail.com>
2023-03-27 18:39:23 +0300

    * add cloud storage

--- services/schedule/src/main/resources/db/migration/V1_1_0__my_first_migration.sql
+++ services/schedule/src/main/resources/db/migration/V1_1_0__my_first_migration.sql
@@ -1,5 +1,8 @@
+CREATE TABLE Customers
+(
+    Id INT,
+    Age INT,
+    FirstName VARCHAR(20),
+    LastName VARCHAR(20)
+);
 
-
-
-insert into schedule_entity (class_id, student_id, subject, teacher_id) values (2, 1, 'Lesson', 2);
-insert into schedule_entity (class_id, student_id, subject, teacher_id) values (5, 2, 'Example', 3);
