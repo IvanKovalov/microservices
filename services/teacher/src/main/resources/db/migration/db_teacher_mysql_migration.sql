dfc44441840d5db44047d212e5fbea0d583bd5d1
DaraKolm <75583812+DaraKolm@users.noreply.github.com>
2023-04-12 14:14:15 +0300

    added sql scripts (create, migration), changed dependencies and plugins in pom according to "add migration" rogoznyi's commit

---
+++ services/teacher/src/main/resources/db/migration/db_teacher_mysql_migration.sql
@@ -0,0 +1,8 @@
+CREATE TABLE if not exists teacher (
+    id INT NOT NULL AUTO_INCREMENT,
+    Surname VARCHAR(100),
+    Name VARCHAR(100),
+    Patronymic VARCHAR(100),
+    Posada VARCHAR(100),
+    PRIMARY KEY (id)
+);
